3fb46a68b40f1734e9b956411cc649e9ad55f054
James <1jamesthompson1@gmail.com>
2023-04-24 15:03:54 +1200

    Set it up for demo

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ shutil.rmtree(download_dir, ignore_errors=True)
 shutil.rmtree(text_dir, ignore_errors=True)
 shutil.rmtree(summarized_dir, ignore_errors=True)
 
-PDFDownloader.downloadPDFs(download_dir, 2000, 2003, 5)
+PDFDownloader.downloadPDFs(download_dir, 2000,2002, 4)
 PDFParser.convertPDFToText(download_dir, text_dir)
 TextSummarizer.summarizeFiles(text_dir, summarized_dir, 5)
