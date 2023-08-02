8c50116b5a823dddd7d63cc79a217bcaa6c30499
James <1jamesthompson1@gmail.com>
2023-08-02 21:16:11 +1200

    Update the openAI calller.
    Add in the model with higher context as well as settings for tempurature.

--- python/OpenAICaller.py
+++ python/OpenAICaller.py
@@ -9,18 +9,19 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 class OpenAICaller:
     def __init__(self):
         self.model = "gpt-3.5-turbo"
-
+        self.model_large = "gpt-3.5-turbo-16k"
     def setup(self):
         # Set up the OpenAI API credentials and other configuration options
         pass
 
-    def query(self, system, user):
+    def query(self, system, user, temp = 1, large_model = True):
         completion = openai.ChatCompletion.create(
-            model=self.model,
+            model= self.model_large if large_model else self.model,
             messages=[
                 {"role": "system", "content": system},
                 {"role": "user", "content": user}
-            ]
+            ],
+            temperature=temp,
         )
 
         return completion.choices[0].message.content
