44a63945064a0b70aa2874c6d519fc8bad92f7f3
James <1jamesthompson1@gmail.com>
2023-09-04 15:56:12 +1200

    Add in No. of matches to the search results. #22

--- viewer/search.py
+++ viewer/search.py
@@ -1,6 +1,25 @@
 import pandas as pd
-import os    
+import os
+
 # import yaml
+
+class Search:
+    def __init__(self, query: str):
+        self.query = query
+
+    def getQuery(self):
+        return self.query
+
+class SearchResult:
+    def __init__(self, report_matches: int, theme_matches: int):
+        self.report_matches = report_matches
+        self.theme_matches = theme_matches
+
+    def include(self):
+        return self.matches() > 0
+    
+    def matches(self):
+        return self.report_matches + self.theme_matches
 
 class Searcher:
     def __init__(self):
@@ -33,13 +52,20 @@ class Searcher:
             if theme_summary == "Not found" and report_text == "Not found":
                 continue
             
-            if not self.search_report(report_text, theme_summary, query):
+            search_result = self.search_report(report_text, theme_summary, Search(query))
+            if not search_result.include():
                 continue
 
             
-            reports.append({"ReportID": dir, "NoFiles": number_of_files, "Theme": theme_summary})
-
-        return pd.DataFrame(reports)
+            reports.append({"ReportID": dir, "NoMatches": search_result.matches(), "Theme": theme_summary})
+        
+        return pd.DataFrame(reports).sort_values(by=['NoMatches'], ascending=False)
     
-    def search_report(self, report_text: str, theme_text: str, query: str) -> bool:
-        return query in report_text or query in theme_text
+    def search_report(self, report_text: str, theme_text: str, search: Search) -> SearchResult:
+        return SearchResult(
+            report_matches = report_text.count(search.getQuery()),
+            theme_matches = theme_text.count(search.getQuery())
+        )
+    
+
+
