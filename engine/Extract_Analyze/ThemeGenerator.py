d3fab69ba9a67b9d7a6ee6da772d5861db029b7b
James Thompson <1jamesthompson1@gmail.com>
2023-12-04 16:07:46 +1300

    55-quote-and-reference-checker
    Add in quote checker and repairer.
    
    This has been integrated with ThemeGeneration but not Weightings as issue #56 has not yet been completed,
    
    * Update the theme generator to output the themes into yaml.
    
    This makes it easier for the reference checking and manipulation of the themes.
    
    * Finishing for the the night
    
    * Basic reference checker.
    
    This will extract and check all of the references both quotes and citations given in a chunk of text.
    
    Currently the theme generator does nothing to fix broken references.
    
    * Add in basic reparing of references.
    
    This means that the referencesChecking now returns either validated referneces, repaired references or unrepairable references.
    
    ThemeGenerator will need to be updated to e able to handle the updated references
    
    * Add in fixing of the references in the Reference checker.
    
    It will now return the reapired text.
    
    * Reenable the theme generator openAI api request.
    
    * Add support for multiple section location per reference.
    
    * Fix small changes causing crashes
    
    * Saving work, this commit is not complete. Add citation repair.
    
    * Update theme propmt to support better formatted quotes.
    
    * Add in invalid message. Finish the citation repairing.
    
    * Make the citation checking softer
    
    * Remove debug logouts

--- engine/Extract_Analyze/ThemeGenerator.py
+++ engine/Extract_Analyze/ThemeGenerator.py
@@ -1,9 +1,10 @@
 import os
+import yaml
 
 from ..OpenAICaller import openAICaller
 from . import OutputFolderReader
 from .Summarizer import ReportExtractor
-from . import Themes
+from . import Themes, ReferenceChecking
 
 class ThemeGenerator:
     def __init__(self, output_folder, report_dir_template, report_theme_template):
@@ -69,7 +70,7 @@ class ThemeGenerator:
 
     def _get_theme(self, report_id, report_text):
 
-        print(f"  Generating themes for report {report_id}")
+        print(f" Generating themes for report {report_id}")
 
         important_text = ReportExtractor(report_text, report_id).extract_important_text()[0]
 
@@ -77,7 +78,22 @@ class ThemeGenerator:
             return
         
         system_message = """
-You will be provided with a document delimited by triple quotes and a question. Your task is to answer the question using only the provided document and to cite the passage(s) of the document used to answer the question. There may be multiple citations needed. If the document does not contain the information needed to answer this question then simply write: "Insufficient information." If an answer to the question is provided, it must include quotes with citation. Use the following format to cite relevant passages "quote in here" (section.paragraph.subparagraph).
+You will be provided with a document delimited by triple quotes and a question. Your task is to answer the question using only the provided document and to cite the passage(s) of the document used to answer the question. There may be multiple citations needed. If the document does not contain the information needed to answer this question then simply write: "Insufficient information." If an answer to the question is provided, it must include quotes with citation.
+
+You must follow these formats exactly.
+For direct quotes there can only ever be one section mentioned:
+"quote in here" (section.paragraph.subparagraph)
+For indirect quotes there may be one section, multiple or a range: 
+sentence in here (section.paragraph.subparagraph)
+sentence in here (section.paragraph.subparagraph, section.paragraph.subparagraph, etc)
+sentence in here (section.paragraph.subparagraph-section.paragraph.subparagraph)
+
+
+Example quotes would be:
+"it was a wednesday afternoon when the boat struck" (5.4)
+It was both the lack of fresh paint and the old radar dish that caused this accident (4.5.2, 5.4.4)
+
+Quotes should be weaved into your answer. 
 """
         user_message = f"""
 '''
@@ -86,7 +102,9 @@ You will be provided with a document delimited by triple quotes and a question. 
 
 Question:
 Please provide me 3 - 6 safety themes that are most related to this accident.
-For each theme provide a paragraph explaining what the theme is and reasoning as to why it is relevant to this accident. Provide evidence for your reasoning with inline quotes. More than 1 quote may be needed.
+For each theme provide a paragraph explaining what the theme is and reasoning as to why it is relevant to this accident. Provide evidence for your reasoning with inline quotes. More than 1 quote may be needed and direct quotes are preferable.
+
+Please output your answer in yaml. There should be no opening or closing code block just straight yaml. The yaml format should have a name and explanation field (which uses a literal scalar block) for each safety theme.
 
 ----
 Here are some definition
@@ -107,27 +125,64 @@ either as Risk Controls or Organisational Influences.
 
 Safety theme - Indication of recurring circumstances or causes, either across transport modes or over time. A safety theme may
 cover a single safety issue, or two or more related safety
-issues.
+issues. 
 """
 
-        report_themes = self.open_ai_caller.query(
+        report_themes_str = self.open_ai_caller.query(
             system_message,
             user_message,
             large_model=True,
             temp = 0
         )
 
-        if report_themes is None:
+        # with open(self._get_theme_file_path(report_id), "r") as f:
+        #     report_themes_str = f.read()
+
+        if report_themes_str is None:
             return
 
-        with open(self._get_theme_file_path(report_id), "w") as f:
-            f.write(report_themes)
+        try :
+            report_themes = yaml.safe_load(report_themes_str)
+        except yaml.YAMLError as exc:
+            print(exc)
+            print("  Error parsing yaml for themes")
+            return self._get_theme(report_id, report_text)
+        
+        print(f"  Themes for {report_id} generated now validating references")
 
-        print(f"  Themes for {report_id} generated")
+        referenceChecker = ReferenceChecking.ReferenceValidator(report_text)
+
+        validated_themes_counter = 0
+        updated_themes_counter = 0
+
+        for theme in report_themes:
+            result = referenceChecker.validate_references(theme['explanation'])
+
+            if result is None:
+                return self._get_theme(report_id, report_text)
+
+            processed_text, num_references, num_updated_references = result
+            updated_themes_counter += num_updated_references
+            if isinstance(processed_text, str):
+                print(f"Updating theme {theme['name']}")
+                theme['explanation'] = processed_text
+
+
+            validated_themes_counter += num_references
+            
+        print(f"    {validated_themes_counter} references validated across {len(report_themes)} themes with {updated_themes_counter} themes updated")
+
+        print(f"  References for {report_id} validated now writing to file")
+
+        with open(self._get_theme_file_path(report_id), "w") as f:
+            yaml.dump(report_themes, f, default_flow_style=False, width=float('inf'), sort_keys=False)
+        
 
     def _read_themes(self, report_id, report_themes):
-        with open(self._get_theme_file_path(report_id), "r") as f:
-            report_themes = f.read()
+        theme = yaml.safe_load(report_themes)
 
-        self.all_themes += (f"Themes for {report_id}: \n{report_themes}\n")
+        # convert theme object with name and explanation to a string
+        theme_str = '\n\n'.join(f"{element['name']}\n{element['explanation']}" for element in theme)
+
+        self.all_themes += (f"Themes for {report_id}: \n{theme_str}\n")
 
