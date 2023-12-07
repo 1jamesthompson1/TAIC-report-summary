8acbe1188fd4443b31f03727d2e8c76d779700f2
1jamesthompson1 <1jamesthompson1@gmail.com>
2023-12-08 11:59:32 +1300

    WeightingComparer has been updated to hanlde new format.
    However now the comparision of weightings explanation needs to be done.

--- engine/Verify/WeightingComparer.py
+++ engine/Verify/WeightingComparer.py
@@ -1,8 +1,9 @@
 from ..Extract_Analyze.OutputFolderReader import OutputFolderReader
 from ..Extract_Analyze.Themes import ThemeReader
-
-
 from .Comparer import Comparer
+
+import yaml
+import csv
 
 class WeightingComparer(Comparer):
     def __init__(self):
@@ -11,13 +12,13 @@ class WeightingComparer(Comparer):
         self.compared_weightings = dict()
 
     def  decode_summaries(self, report_summary):
-        elements = report_summary.split(",")
-        num_themes = ThemeReader().get_num_themes()
-        pages_read = set(elements[1].strip('[]').split("  "))
-        weightings = [float(x) for x in elements[2:(num_themes+2)]]
+        csv_reader = csv.reader([report_summary])
 
-        return {"weights": weightings, "pages_read": pages_read}
+        elements = next(csv_reader)[:-2]
 
+        return {"weights": [float(weight) if weight !="<NA>" else None for weight in elements[2::3]],
+                "explanation": elements[3::3],
+                "pages_read": set(elements[1].strip('[]').split("  "))}
 
     def add_report_ID(self, report_id, report_summary):
         self.validation_set[report_id] = self.decode_summaries(report_summary)
@@ -51,18 +52,35 @@ class WeightingComparer(Comparer):
         pages_read_jaccard_similarity = len(validation_pages_read.intersection(engine_pages_read)) / len(validation_pages_read.union(engine_pages_read))
         
         # Compare the weightings
-        
+                
         validation_weightings = self.validation_set[report_id]["weights"]
-
+        validation_explanation = self.validation_set[report_id]["explanation"]
         engine_weightings = engine_summary["weights"]
+        engine_explanation = engine_summary["explanation"]
 
         if len(validation_weightings) != len(engine_weightings):
             print(f"  Validation weightings and engine weightings have different lengths. Skipping {report_id}")
             return
+
+        ## Make sure that None are in the same location
+        none_in_both = [i for i, (v, e) in enumerate(zip(validation_weightings, engine_weightings)) if v is None and e is None]
+        none_in_one = [i for i, (v, e) in enumerate(zip(validation_weightings, engine_weightings)) if (v is None) != (e is None)]
+
+        if none_in_one:
+            print(f"  Validation weightings and engine weightings have a None in a different location {none_in_one}. Skipping {report_id}")
+            return
+
+        print(f"  Validation weightings and engine weightings have a None in the same location {none_in_both}. Removing from comparison.")
+
+        validation_weightings = [v for i, v in enumerate(validation_weightings) if i not in none_in_both]
+        engine_weightings = [e for i, e in enumerate(engine_weightings) if i not in none_in_both]
+        validation_explanation = [v for i, v in enumerate(validation_explanation) if i not in none_in_both]
+        engine_explanation = [e for i, e in enumerate(engine_explanation) if i not in none_in_both]
         
         manhattan_weightings_similarity = sum([abs(validation_weightings[i] - engine_weightings[i]) for i in range(len(validation_weightings))])/ThemeReader().get_num_themes()
 
         self.compared_weightings[report_id] = {"pages_read": pages_read_jaccard_similarity, "weightings": manhattan_weightings_similarity}
 
-
+        # TODO
+        # Compare the explanations
 
