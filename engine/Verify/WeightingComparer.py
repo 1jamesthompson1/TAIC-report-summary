a528bb77ea150264a7edf65c49dcf82799126fde
1jamesthompson1 <1jamesthompson1@gmail.com>
2023-12-08 15:46:04 +1300

    Add basic support for full weightings comparision with explanations being compared. Working on #80

--- engine/Verify/WeightingComparer.py
+++ engine/Verify/WeightingComparer.py
@@ -1,6 +1,8 @@
 from ..Extract_Analyze.OutputFolderReader import OutputFolderReader
 from ..Extract_Analyze.Themes import ThemeReader
 from .Comparer import Comparer
+from ..OpenAICaller import openAICaller
+from ..Modes import *
 
 import yaml
 import csv
@@ -9,7 +11,7 @@ class WeightingComparer(Comparer):
     def __init__(self):
         super().__init__()
         self.get_validation_set('summaries')
-        self.compared_weightings = dict()
+        self.compared_summaries = dict()
 
     def  decode_summaries(self, report_summary):
         csv_reader = csv.reader([report_summary])
@@ -30,12 +32,13 @@ class WeightingComparer(Comparer):
 
         print("Finished comparing weightings.")
         
-        num_reports = len(self.compared_weightings)
+        num_reports = len(self.compared_summaries)
         print('==Validation summary==')
         print(f"  {num_reports} reports compared.")
         # print(f"  {[report for report in self.compared_weightings]}")
-        print(f"  Average weighting manhattan distance: {sum([self.compared_weightings[report]['weightings'] for report in self.compared_weightings])/num_reports}")
-        print(f"  Average pages read jaccard similarity: {sum([self.compared_weightings[report]['pages_read'] for report in self.compared_weightings])/num_reports}")
+        print(f"  Average weighting manhattan distance: {sum([self.compared_summaries[report]['weightings'] for report in self.compared_summaries])/num_reports}")
+        print(f"  Average pages read jaccard similarity: {sum([self.compared_summaries[report]['pages_read'] for report in self.compared_summaries])/num_reports}")
+        print(f"  Average explanation similarity: {sum([self.compared_summaries[report]['explanation'] for report in self.compared_summaries])/num_reports}")
     
 
     def compare_two_summaries(self, report_id, report_summary):
@@ -79,8 +82,79 @@ class WeightingComparer(Comparer):
         
         manhattan_weightings_similarity = sum([abs(validation_weightings[i] - engine_weightings[i]) for i in range(len(validation_weightings))])/ThemeReader().get_num_themes()
 
-        self.compared_weightings[report_id] = {"pages_read": pages_read_jaccard_similarity, "weightings": manhattan_weightings_similarity}
-
-        # TODO
         # Compare the explanations
+        explanation_similarity = list()
+        for theme, validation_explanation, engine_explanation in zip(ThemeReader(modes = get_report_mode_from_id(report_id))._themes, validation_explanation, engine_explanation):
+            explanation_similarity.append(self.compare_weighting_reasoning(theme, validation_explanation, engine_explanation))
 
+        self.compared_summaries[report_id] = {"pages_read": pages_read_jaccard_similarity, 
+                                       "weightings": manhattan_weightings_similarity,
+                                       "explanation": sum(explanation_similarity)/len(explanation_similarity)}
+
+    def compare_weighting_reasoning(self, theme, validation_explanation, engine_explanation):
+        system_message = """
+I need you to help me compare two blocks of text.
+
+Both texts to be compared with be given sourrounded by triple quotes.
+
+Below that will be a question specifc to the comparision of these two texts.
+
+Your response will be a percentage of how simliiar these texts are.
+Just return a number from 0-100. With 0 being nothing alike and 100 being exactly the same.
+"""
+        
+        user_message = f"""
+'''
+{validation_explanation}
+'''
+
+'''
+{engine_explanation}
+'''
+
+Question:
+
+Above are two explanations of how much {theme['title']} is related and contributory to a specifc accident.
+
+I want to know how similiar the reasoning is.
+
+{theme['title']} is defined as such:
+{theme['description']}
+
+Here are some general definitions:
+
+Safety factor - Any (non-trivial) events or conditions, which increases safety risk. If they occurred in the future, these would
+increase the likelihood of an occurrence, and/or the
+severity of any adverse consequences associated with the
+occurrence.
+
+Safety issue - A safety factor that:
+• can reasonably be regarded as having the
+potential to adversely affect the safety of future
+operations, and
+• is characteristic of an organisation, a system, or an
+operational environment at a specific point in time.
+Safety Issues are derived from safety factors classified
+either as Risk Controls or Organisational Influences.
+
+Safety theme - Indication of recurring circumstances or causes, either across transport modes or over time. A safety theme may
+cover a single safety issue, or two or more related safety
+issues. 
+"""
+        while(True):
+            responses = openAICaller.query(
+                system_message,
+                user_message,
+                large_model=True,
+                temp = 0,
+                n = 5
+            )
+
+            try :
+                average_percent = sum([float(response) for response in responses])/5
+                if average_percent < 0 or average_percent > 100:
+                    raise ValueError
+                return average_percent
+            except ValueError:
+                print(f"  Invalid repsonse from model: {responses}")
+                continue
