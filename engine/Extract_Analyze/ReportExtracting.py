48e3b09380364731bfb343f2105851cd3babf90a
James Thompson <1jamesthompson1@gmail.com>
2024-03-27 10:34:14 +1300

    129 infer safety issues when no regex safety issues are found (#133)

--- engine/Extract_Analyze/ReportExtracting.py
+++ engine/Extract_Analyze/ReportExtracting.py
@@ -99,7 +99,11 @@ class ReportExtractor:
         return pages_to_read
 
     def extract_section(self, section_str: str):
-        base_regex_template = lambda section: fr"(( {section}) {{1,3}}(?![\s\S]*^{section}))|((^{section}) {{1,3}})(?![\S\s()]{{1,100}}\.{{2,}})"
+        """
+        This function extract a numbered section from the report.
+        You need to give it a string like 5, 5.1, 5.1.1, etc. It can struggle with the last or second to last section in the report. In this case it utilses AI
+        """
+        base_regex_template = lambda section: fr"(?<!\.{{3,}} {{0,4}}\d{{1,3}} ?\s)((( {section}) {{1,3}}(?![\s\S]*^{section}))|((^{section}) {{1,3}}))(?![\S\s()]{{1,100}}\.{{2,}})"
 
         split_section = section_str.split(".")
         section = split_section[0]
@@ -130,7 +134,10 @@ class ReportExtractor:
                 break
 
         if startMatch == None or endMatch == None:
-            return None
+            print("Warning: could not find section")
+            print(f"  startMatch: {startMatch} with regex {startRegex} \n  endMatch: {endMatch} with regex {endRegex}")
+            print("  Attempting to extract section using page numbers")
+            return self.__extract_section_using_LLM(section_str)
 
         if endMatch.end() < startMatch.end():
             print(f"Error: endMatch is before startMatch")
@@ -145,11 +152,100 @@ class ReportExtractor:
         print(f"Error: could not find section")
         return None
     
-    def extract_safety_issues(self):
+    def __extract_section_using_LLM(self, section):
         """
-        Safety issues representation vary throughout the reports.
+        A helper function to extract_section that will read the content section and find the page numbers then extract it from there.
         """
 
+        content_section = self.extract_contents_section()
+
+        pages = openAICaller.query(
+            """
+            You are helping me read a content section.
+
+I will send you a content section and a section and you will return the pages with which that section will cover.
+
+Your response is only a list of integers. No words are allowed in your response. e.g '12,45' or '10,23'. If you cant find the section number given then just return "None".
+            """,
+            f"""
+'''
+{content_section}
+'''
+
+The section number I am looking for is {section}
+            """,
+            large_model=True,
+            temp = 0)
+        
+        if pages == "None":
+            print(f"  Failed to find the section using the LLM, it responded with '{pages}'. The search was for section '{section}'")            
+            return None
+        
+        print(" Found the section using the LLM" + pages)
+
+        pages_to_read = [int(num) for num in pages.split(",")]
+
+        # Make the array every page between first and last
+        pages_to_read = list(range(pages_to_read[0], pages_to_read[-1] + 1))
+
+        # Retrieve that actual text for the page numbers.
+        section_text = self.extract_text_between_page_numbers(pages_to_read[0], pages_to_read[-1])
+
+        return section_text
+
+class SafetyIssuesAndRecommendationsExtractor(ReportExtractor):
+    def __init__(self, report_text, report_id):
+        super().__init__(report_text, report_id)
+
+    def extract_recommendation_section(self):
+        content_section = self.extract_contents_section()
+        
+        if content_section == None:
+            print(f'  Without content section the recommendation section cannot be found')
+            return None
+        
+        add_whitespace = lambda text: r"\s{0,2}".join(text)
+
+        search_regex = rf'(\d{{1,3}})\s{{0,2}}\.?\s{{0,2}}(({add_whitespace("safety")})?\s?{add_whitespace("recommendations")}?).*?(\d{{1,3}})'
+
+        recommendation_match = re.search(search_regex, content_section, re.IGNORECASE)
+
+        if recommendation_match == None:
+            print(f'  Could not find the recommendation section')
+            return None
+        
+        print(f'  Found the recommendation section it was {recommendation_match.group(1)}')
+        
+        recommendation_section = self.extract_section(recommendation_match.group(1))
+
+        return  recommendation_section
+
+        
+    
+    def extract_safety_issues(self):
+        """
+        Extract safety issues from a report.
+        """
+
+        safety_issues = self.__extract_safety_issues_with_regex()
+        exact_safety_issues = True
+
+        if safety_issues == None:
+            exact_safety_issues = False
+            safety_issues = self.__extract_safety_issues_with_inference()
+
+        # Add the quality of safety issue to the yaml
+        if safety_issues == None:
+            return None
+        
+        safety_issues_with_quality = [{"safety_issue": issue, "quality": "exact" if exact_safety_issues else "inferred"} for issue in safety_issues ]
+
+        return safety_issues_with_quality
+        
+    def __extract_safety_issues_with_regex(self):
+        """
+        Search for safety issues using regex
+        """
         safety_regex = r's ?a ?f ?e ?t ?y ? ?i ?s ?s ?u ?e ?s?'
         end_regex = r'([\s\S]*?)(?=(\d+\.(\d+\.)?(\d+)?)|(^ [A-Z]))'
         preamble_regex = r'([\s\S]{50})'
@@ -168,6 +264,7 @@ class ReportExtractor:
         for regex in safety_issues_regexes:
             if len(safety_issue_matches) > 0 and regex.search(self.report_text):
                 print("Error: multiple regexes matched")
+                return None
 
             if len(safety_issue_matches) == 0 and regex.search(self.report_text):
                 safety_issue_matches.extend(regex.findall(self.report_text))
@@ -177,6 +274,10 @@ class ReportExtractor:
 
         ## Remove excess whitespace
         safety_issues_removed_whitespace = [issue.strip().replace("\n", " ") for issue in safety_issues_uncleaned]
+
+        if len(safety_issues_removed_whitespace) == 0:
+            print("  Could not find any safety issues using regex.")
+            return None
 
         ## Clean up characters with llm
         clean_text = lambda text: openAICaller.query(
@@ -196,7 +297,100 @@ Please just return the cleaned version of the text. Without starting with Safety
         safety_issues_cleaned = [clean_text(issue) for issue in safety_issues_removed_whitespace]
 
         return safety_issues_cleaned
+
+    def __extract_safety_issues_with_inference(self):
+        """
+        Search for safety issues using inference from GPT 4 turbo.
+        """
+
+        important_text, pages_to_read = self.extract_important_text()
+
+        if important_text == None:
+            return None
         
+        message = lambda text: f'''
+{text}
+        
+=Instructions=
+
+I want to know the safety issues which this investigation has found.
+
+Can your response please be in yaml format.
+
+- |
+    bla bla bla
+- |
+    bla bla bla bla
+
+There is no need to enclose the yaml in any tags.
+
+=Here are some definitions=
+
+Safety factor - Any (non-trivial) events or conditions, which increases safety risk. If they occurred in the future, these would
+increase the likelihood of an occurrence, and/or the
+severity of any adverse consequences associated with the
+occurrence.
+
+Safety issue - A safety factor that:
+• can reasonably be regarded as having the
+potential to adversely affect the safety of future
+operations, and
+• is characteristic of an organisation, a system, or an
+operational environment at a specific point in time.
+Safety Issues are derived from safety factors classified
+either as Risk Controls or Organisational Influences.
+
+Safety theme - Indication of recurring circumstances or causes, either across transport modes or over time. A safety theme may
+cover a single safety issue, or two or more related safety
+issues.
+'''
+        
+        response = openAICaller.query(
+            """
+You are going help me read a transport accident investigation report.
+
+ I want you to please read the report and respond with the safety issues identified in the report.
+
+Please only respond with safety issues that are quite clearly stated and/or implied.
+
+It should be noted that the number of safety issues in a report has a minimum of 1 an 0.25 quantile of 1, median of 2, 0.75 quantile of 3 and a maximum of 13. You should try make your answers match this distribution and on average a report will have only 2 safety issues.
+
+Remember the definitions give
+
+Safety factor - Any (non-trivial) events or conditions, which increases safety risk. If they occurred in the future, these would
+increase the likelihood of an occurrence, and/or the
+severity of any adverse consequences associated with the
+occurrence.
+
+Safety issue - A safety factor that:
+• can reasonably be regarded as having the
+potential to adversely affect the safety of future
+operations, and
+• is characteristic of an organisation, a system, or an
+operational environment at a specific point in time.
+Safety Issues are derived from safety factors classified
+either as Risk Controls or Organisational Influences.
+
+Safety theme - Indication of recurring circumstances or causes, either across transport modes or over time. A safety theme may
+cover a single safety issue, or two or more related safety
+issues.
+            """,
+            message(important_text),
+            large_model=True,
+            temp=0)
+        
+        if response[:7] == '"""yaml' or response[:7] == '```yaml':
+            response = response[7:-3]
+        
+        try:
+            safety_issues = yaml.safe_load(response)
+        except yaml.YAMLError as exc:
+            print(exc)
+            print("  Assuming that there are no safety issues in the report.")
+            return None
+        
+        return safety_issues
+
 
 class ReportExtractingProcessor:
 
@@ -207,7 +401,7 @@ class ReportExtractingProcessor:
         self.file_name_template = file_name_template
         self.refresh = refresh
 
-    def _output_safety_issues(self, report_id, report_text):
+    def __output_safety_issues(self, report_id, report_text):
 
         print("  Extracting safety issues from " + report_id)
 
@@ -220,7 +414,7 @@ class ReportExtractingProcessor:
             print(f"   {output_path} already exists")
             return
 
-        safety_issues = ReportExtractor(report_text, report_id).extract_safety_issues()
+        safety_issues = SafetyIssuesAndRecommendationsExtractor(report_text, report_id).extract_safety_issues()
 
         if safety_issues == None:
             print(f"  Could not extract safety issues from {report_id}")
@@ -232,6 +426,6 @@ class ReportExtractingProcessor:
             yaml.safe_dump(safety_issues, f, default_flow_style=False, width=float('inf'), sort_keys=False)
 
     def extract_safety_issues_from_reports(self):
-        self.output_folder_reader.process_reports(self._output_safety_issues)
+        self.output_folder_reader.process_reports(self.__output_safety_issues)
 
         
