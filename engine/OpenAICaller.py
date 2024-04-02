54b3a771f0f2d111a4cba70b02fb34ceb6a214aa
1jamesthompson1 <1jamesthompson1@gmail.com>
2024-04-02 16:32:34 +1300

    Update to handle too many token error properly in openAI caller. Add suport for finetuned models

--- engine/OpenAICaller.py
+++ engine/OpenAICaller.py
@@ -1,7 +1,8 @@
 from math import exp
 import time
 import os
-from openai import OpenAI
+import openai
+from openai import OpenAI, BadRequestError
 from dotenv import load_dotenv
 import tiktoken
 
@@ -13,43 +14,53 @@ client = OpenAI(
 
 class OpenAICaller:
     def __init__(self):
-        self.model = "gpt-3.5-turbo-16k"
+        # Limits have to be manually set as API has no way of retrieving them.
+        self.models = {
+            'gpt-3.5': {"name": "gpt-3.5-turbo-16k", "limit": 16_385},
+            'gpt-4': {"name": "gpt-4-0125-preview", "limit": 128_000},
+            'gpt-3.5-ft-SIExtraction': {"name": "ft:gpt-3.5-turbo-0125:personal::97ZC9cF4", "limit": 16_385},
+        }
+
+        self.model_default = self.models['gpt-3.5']
+
+
+    def query(self, system, user, temp = 1, model = "gpt-3.5", n = 1):
+
+        if model not in self.models:
+            print(f"Model {model} not found, using default model")
+            model = self.model_default
         
-        self.model_large = "gpt-4-1106-preview"
-    def setup(self):
-        # Set up the OpenAI API credentials and other configuration options
-        pass
+        selected_model = self.models[model]
 
-    def query(self, system, user, temp = 1, large_model = False, n = 1):
-
-        model= self.model_large if large_model else self.model
-
-        # Check to make sure there arent too many tokens
-        total_length = sum(self.get_tokens(model, [system, user]))+1
+        total_length = sum(self.get_tokens([system, user]))
         
         # This is hardcoded due to there being no API way of seeing if there are too many tokens.
-        if (large_model and (total_length > 128000)) or ( (not large_model) and (total_length > 16000)):
+        if total_length > selected_model['limit']:
             print(f"Too many tokens {total_length}, not sending to OpenAI")
             return None
 
         # If rate limit error happens then just wait a minute and try again
-        completion = client.chat.completions.create(
-        model = model,
-        messages=[
-            {"role": "system", "content": system},
-            {"role": "user", "content": user}
-        ],
-        temperature=temp, n = n,
-        max_tokens=2000,
-        )
+        try:
+            completion = client.chat.completions.create(
+            model = selected_model['name'],
+            messages=[
+                {"role": "system", "content": system},
+                {"role": "user", "content": user}
+            ],
+            temperature=temp, n = n,
+            )
+        except openai.BadRequestError as e:
+            print(f'Too many tokens in request OpenAI declined:\n {e}')
+            return None
 
         if n == 1:
             return completion.choices[0].message.content
         else:
             return [choice.message.content for choice in completion.choices]
         
-    def get_tokens(self, model, texts):
-        enc = tiktoken.encoding_for_model(model)
+    def get_tokens(self, texts):
+        # No need to check for model as all models have the same encoding
+        enc = tiktoken.encoding_for_model("gpt-3.5-turbo-16k")
         return [len(enc.encode(text)) for text in texts]
         
             
