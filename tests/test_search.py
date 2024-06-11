bdd9445670c3a1ec659cad02b4eb91e200ff10cb
James Thompson <1jamesthompson1@gmail.com>
2024-06-11 15:56:47 +1200

    Uncomplete 170-major-revamp-of-viewr-app-integrating-in-new-searcher-from-165 (#173)
    * Update the searching of the reports
    
    * Update the webapp to work now with the new searching module.
    
    * Add in filtering based off year and mode.
    
    * Update search to handle a single mode filter
    
    * update webapp search to use the same search as the notebook.
    
    It isn't the cleanest. However time should not be wasted here as the vectorDB may change.
    
    * Final remove restriction on year for safety issues search
    
    * Update the dependencies

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,209 +1,112 @@
-import viewer.Search as Search
+import viewer.Searching as Searching
 
+import engine.Modes as Modes
 
-class TestRegexCreation:
-    
-    def setup_method(self):
-        self.searcher = Search.Searcher()
+import pandas as pd
+import pytest
 
-    
-    def test_basic(self):
-        query = "hello"
-        regex = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        assert regex[0].pattern == r'hello'
+class TestSearchSettings:
 
-    def test_basic_exact(self):
-        query = '"hello"'
-        regex = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        assert regex[0].pattern == r'\b(hello)\b'
+    def test_basic_creation(self):
+        settings = Searching.SearchSettings([Modes.Mode.a, Modes.Mode.r], (2000, 2020))
 
-    def test_basic_or_word(self):
-        query = "hello OR world"
-        regex = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        assert regex[0].pattern == r'hello|world'
+        assert settings.getModes() == [Modes.Mode.a, Modes.Mode.r]
+        assert settings.getYearRange() == (2000, 2020)
 
-    def test_basic_or_symbol(self):
-        query = "hello | world"
-        regex = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        assert regex[0].pattern == r'hello|world'
+    def test_failed_creation(self):
+        with pytest.raises(TypeError):
+            settings = Searching.SearchSettings([Modes.Mode.a, Modes.Mode.r], (2001, "2020"))
 
-    def test_basic_exclusion(self):
-        query = "hello -world"
-        regex = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        assert regex[0].pattern == r'hello(?! world)'
+        with pytest.raises(TypeError):
+            settings = Searching.SearchSettings(['a', 'r'], (2001, 2020))
+        
+class TestSearch:
 
-    def test_prior_exclusion(self):
-        query = "-not hello world"
-        regex = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        assert regex[0].pattern == r'(?<!not )hello world'
+    def test_basic_creation(self):
+        search = Searching.Search("hello", Searching.SearchSettings([Modes.Mode.a, Modes.Mode.r], (2000, 2020)))
 
-    def test_basic_wildcard(self):
-        query = "hello*"
-        regex = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        assert regex[0].pattern == r'hello.*'
+        assert search.getQuery() == "hello"
+        assert search.getSettings().getModes() == [Modes.Mode.a, Modes.Mode.r]
+        assert search.getSettings().getYearRange() == (2000, 2020)
 
-    def test_basic_and(self):
-        query = "hello AND world"
-        regexes = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        regex_patterns = [regex.pattern for regex in regexes]
-        assert regex_patterns == [r'hello', r'world']
+    def test_from_form_creation(self):
 
-    def test_and_or_combination(self):
-        query = "hello AND world OR goodbye"
-        regexes = Search.Searcher.get_regex(Search.Search(query, {'use_synonyms': False}))
-        regex_patterns = [regex.pattern for regex in regexes]
-        assert regex_patterns == [r'hello', r'world|goodbye']
-    
-class TestSearchReport:
-
-    def setup_method(self):
-        self.searcher = Search.Searcher()
-
-        self.example_report = """
-This is a boat crash.
-
-A boat crashed into a bridge.
-
-It crashed into the bridge because the driver was drunk.
-
-The driver almost drowned. The driver was not wearing a life jackets.
-"""
-        self.example_theme = """
-Driving a vessel while drunk is dangerous.
-
-Driving a boat while drunk is illegal.
-
-Life jackets are important.
-"""
-        self.example_reasoning = [
-            "This accident was caused by the driver being drunk. This is means it was the sole cause of the accident.",
-            "The driver was not wearing a life jacket. This is means it was a contributing factor to the consequences of the accident."
-        ]
-
-        self.settings = {
-            'use_synonyms': False,
-            'search_report_text': True,
-            'search_theme_text': True,
-            'search_weighting_reasoning': True,
-            'include_incomplete_reports': True,
-        }
-    
-    def test_non_search(self):
-        result = self.searcher.search_report(self.example_report, self.example_theme, self.example_reasoning, Search.Search("", self.settings))
-        assert result.matches == {}
-
-    def test_basic_search(self):
-        result = self.searcher.search_report(self.example_report, self.example_theme, self.example_reasoning, Search.Search("boat", self.settings))
-        assert result.matches == {
-            'report_result': [2],
-            'theme_result': [1],
-            'weighting_reasoning_result': [0]
+        form = {
+            "searchQuery": "hello",
+            "includeModeAviation": "on",
+            "includeModeRail": "on",
+            "yearSlider-min": "2000",
+            "yearSlider-max": "2020"
         }
 
-    def test_basic_and_search(self):
-        result = self.searcher.search_report(self.example_report, self.example_theme, self.example_reasoning, Search.Search("boat AND drunk", self.settings))
-        assert result.matches == {
-            'report_result': [2,1],
-            'theme_result': [1,2],
-            'weighting_reasoning_result': [0,1]
+        search = Searching.Search.from_form(form)
+
+        assert search.getQuery() == "hello"
+        assert search.getSettings().getModes() == [Modes.Mode.a, Modes.Mode.r]
+        assert search.getSettings().getYearRange() == (2000, 2020)
+
+    def test_failed_form_creation(self):
+
+        form = {
+            "query": "hello"
         }
 
-class TestSearchResult:
+        with pytest.raises(ValueError):
+            search = Searching.Search.from_form(form)
+
+class TestSearcher:
+
+    searcher = Searching.SearchEngine('./viewer/vector_db')
+
+    def test_search(self):
+        search = Searching.Search("hello", Searching.SearchSettings([Modes.Mode.a, Modes.Mode.r], (2000, 2020)))
+        result = self.searcher.search(search, with_rag = False)
+
+        assert result
+        assert result.getSummary() is None
+        assert isinstance(result.getContext(), pd.DataFrame)
+
+    def test_search_with_summary(self):
+        search = Searching.Search("pilot incapacity", Searching.SearchSettings(Modes.all_modes, (2000, 2020)))
+        result = self.searcher.search(search, with_rag = True)
+
+        assert result
+        assert isinstance(result.getSummary(), str)
+        assert isinstance(result.getContext(), pd.DataFrame)
+
+    def test_filtered_search(self):
+        search = Searching.Search("pilot", Searching.SearchSettings([Modes.Mode.a, Modes.Mode.r], (2010, 2015)))
+        result = self.searcher.search(search, with_rag = False)
+
+        assert result
+        assert result.getSummary() is None
+        assert isinstance(result.getContext(), pd.DataFrame)
+
+        assert result.getContext()['year'].isin([2010, 2011, 2012, 2013, 2014, 2015]).all()
+
+        assert result.getContext()['mode'].isin([0, 1]).all()
     
-    def test_simple_include(self):
-        result = Search.SearchResult({
-            'report_result': [2],
-            'theme_result': [1],
-            'weighting_reasoning_result': [1]
-        })
+    def test_filtered_search_single_mode(self):
+        search = Searching.Search("pilot", Searching.SearchSettings([Modes.Mode.a], (2010, 2015)))
+        result = self.searcher.search(search, with_rag = False)
 
-        assert result.include() == True
-    
-    def test_simple_exclude(self):
-        result = Search.SearchResult({
-            'report_result': [0],
-            'theme_result': [0],
-            'weighting_reasoning_result': [0]
-        })
+        assert result
+        assert result.getSummary() is None
+        assert isinstance(result.getContext(), pd.DataFrame)
 
-        assert result.include() == False
+        assert result.getContext()['year'].isin([2010, 2011, 2012, 2013, 2014, 2015]).all()
 
-    def test_and_query_include(self):
-        result = Search.SearchResult({
-            'report_result': [2,1],
-            'theme_result': [1,2],
-            'weighting_reasoning_result': [0,1]
-        })
+        assert result.getContext()['mode'].isin([0]).all()
 
-        assert result.include() == True
+    def test_filtered_search_no_query(self):
+        search = Searching.Search("", Searching.SearchSettings([Modes.Mode.m, Modes.Mode.r], (2002, 2005)))
 
-    def test_and_query_exclude(self):
-        result = Search.SearchResult({
-            'report_result': [2,0],
-            'theme_result': [1,0],
-            'weighting_reasoning_result': [0,0]
-        })
+        result = self.searcher.search(search, with_rag = False)
 
-        assert result.include() == False
+        assert result
+        assert result.getSummary() is None
+        assert isinstance(result.getContext(), pd.DataFrame)
 
-class TestHighlighting:
-    
-    def setup_method(self):
-        self.searcher = Search.Searcher()
+        assert result.getContext()['year'].isin([2002, 2003, 2004, 2005]).all()
 
-        self.example_report = """
-This is a boat crash.
-
-A boat crashed into a bridge.
-
-It crashed into the bridge because the driver was drunk.
-
-The driver almost drowned. The driver was not wearing a life jackets.
-"""
-    
-    def test_basic_highlight(self):
-        regexes = Search.Searcher.get_regex(Search.Search("boat", {'use_synonyms': False}))
-
-        highlights = self.searcher.highlight_matches(self.example_report, regexes)
-
-
-        assert highlights == """
-This is a <span class="match-highlight">boat</span> crash.
-
-A <span class="match-highlight">boat</span> crashed into a bridge.
-
-It crashed into the bridge because the driver was drunk.
-
-The driver almost drowned. The driver was not wearing a life jackets.
-"""
-    def test_no_query_highlight(self):
-        regexes = Search.Searcher.get_regex(Search.Search("", {'use_synonyms': False}))
-
-        highlights = self.searcher.highlight_matches(self.example_report, regexes)
-
-
-        assert highlights == self.example_report
-
-    def test_and_query_highlight(self):
-        regexes = Search.Searcher.get_regex(Search.Search("boat AND drunk", {'use_synonyms': False}))
-
-        highlights = self.searcher.highlight_matches(self.example_report, regexes)
-
-
-        assert highlights == """
-This is a <span class="match-highlight">boat</span> crash.
-
-A <span class="match-highlight">boat</span> crashed into a bridge.
-
-It crashed into the bridge because the driver was <span class="match-highlight">drunk</span>.
-
-The driver almost drowned. The driver was not wearing a life jackets.
-"""
-
-    def test_no_highlighting(self):
-        regexes = Search.Searcher.get_regex(Search.Search("pizza", {'use_synonyms': False}))
-
-        highlights = self.searcher.highlight_matches(self.example_report, regexes)
-
-
-        assert highlights == self.example_report
+        assert result.getContext()['mode'].isin([2, 1]).all()
---
+++ viewer/Searching.py
@@ -0,0 +1,252 @@
+
+# Local
+from engine.OpenAICaller import openAICaller
+import engine.Modes as Modes
+
+# Third party
+import pandas as pd
+import lancedb
+import voyageai
+
+# built in
+from typing import Callable
+
+
+class SearchSettings:
+    def __init__(self, modes: list[Modes.Mode], year_range: tuple[int, int]):
+        """
+        Initializes a new instance of the SearchSettings class.
+        These are all the settings that the `Search` class will have that the `Searcher` class will use.
+
+        Parameters:
+            modes (List[Modes.Mode]): A list of Modes.Mode objects representing the modes to be included in the search.
+            year_range (Tuple[int, int]): A tuple representing the range of years to be included in the search.
+
+        """
+        if not isinstance(year_range[0], int) or not isinstance(year_range[1], int):
+            raise TypeError("year_range must be a tuple of integers")
+        self.year_range = year_range
+        
+        if not isinstance(modes, list) or not all(isinstance(mode, Modes.Mode) for mode in modes):
+            raise TypeError("modes must be a list of Modes.Mode objects")
+        self.modes = modes
+
+    def getYearRange(self) -> tuple[int, int]:
+        return self.year_range
+
+    def getModes(self) -> list[Modes.Mode]:
+        return self.modes
+
+class Search:
+    def __init__(self, query: str, settings: SearchSettings):
+        self.query = query
+        self.settings = settings
+    @classmethod
+    def from_form(cls, form: dict):
+        if form is None or not isinstance(form, dict):
+            raise TypeError(f"Form data is not a dictionary but {type(form)}")
+        if len(form) == 0:
+            raise ValueError(f"Form data is empty")
+
+        try:
+            # Query
+            search_query = form['searchQuery']
+
+            # Modes
+            modes_list = list()
+
+            if 'includeModeAviation' in form.keys():
+                modes_list.append(Modes.Mode.a)
+            if 'includeModeRail' in form.keys():
+                modes_list.append(Modes.Mode.r)
+            if 'includeModeMarine' in form.keys():
+                modes_list.append(Modes.Mode.m)
+
+            # Year
+            year_range = int(form.get('yearSlider-min')), int(form.get('yearSlider-max'))
+            return cls(search_query, settings = SearchSettings(modes_list, year_range))
+        except KeyError as e:
+            raise ValueError(f"Form data is missing key: {e}")
+
+
+    def getQuery(self) -> str:
+        return self.query
+    
+    def getSettings(self) -> SearchSettings:
+        return self.settings
+
+class SearchResult:
+    def __init__(self, context: pd.DataFrame, summary: str = None):
+        self.context = context
+        self.summary = summary
+
+    def getContext(self) -> pd.DataFrame:
+        return self.context
+    
+    def getContextCleaned(self) -> pd.DataFrame:
+        """
+        This method retrieves the context dataframe and makes sure that it has a standard format.
+        """
+        context_df = self.getContext().copy()
+
+        context_df.rename(columns = {'si': 'safety_issue', 'section_relevance_score': 'relevance'}, inplace = True)
+
+        context_df = context_df[['relevance', 'report_id', 'safety_issue_id', 'safety_issue', 'year', 'mode']]
+        return context_df
+    def getSummary(self) -> str | None:
+        return self.summary
+
+class SearchEngine:
+    def __init__(self, db_uri: str):
+        self.db = lancedb.connect(db_uri)
+
+        self.si_table = self.db.open_table('safety_issue_embeddings')
+        self.report_sections_table = self.db.open_table('report_section_embeddings')
+
+        self.vo = voyageai.Client()
+
+    def search(self, search: Search, with_rag = True) -> SearchResult:
+        '''
+        This function takes a search object with some parameters and will create the right `SearchEngineSearcher`
+        '''
+
+        searchEngineSearcher = SearchEngineSearcher(search, self.si_table, self.report_sections_table, self.vo)
+
+        if with_rag and search.getQuery() != "":
+            return searchEngineSearcher.rag_search()
+        else:
+            results = searchEngineSearcher.safety_issue_search_with_report_relevance()
+            return SearchResult(results, None)
+    
+class SearchEngineSearcher:
+    def __init__(self, search: Search, si_table: lancedb.table.Table, report_sections_table: lancedb.table.Table, vo: voyageai.Client):
+        self.query = search.getQuery()
+        self.settings = search.getSettings()
+
+        self.report_sections_table = report_sections_table
+        self.si_table = si_table
+
+        self.vo = vo
+
+    def _embed_query(self, query: str) -> list[float]:
+
+        return self.vo.embed(query, model="voyage-large-2-instruct", input_type="query", truncation=False).embeddings[0]
+        
+    def _table_search(self, filter: str, table: lancedb.table.Table, limit = 100, type: str = ['hybrid', 'fts', 'vector']) -> pd.DataFrame:
+
+        if type == 'hybrid':
+            results = table.search((self._embed_query(self.query), self.query),  query_type='hybrid') \
+                .metric("cosine") \
+                .where(filter, prefilter=True) \
+                .limit(limit) \
+                .to_pandas()
+            results.rename(columns={'_relevance_score': 'section_relevance_score'}, inplace = True)
+        elif type == 'fts':
+            results = table.search(self.query,  query_type='fts') \
+                .limit(limit) \
+                .where(filter, prefilter=True) \
+                .to_pandas()
+            results.rename(columns={'score': 'section_relevance_score'}, inplace = True)
+        else: # type == 'vector'
+            results = table.search(self._embed_query(self.query),  query_type='vector') \
+                .metric("cosine") \
+                .limit(limit) \
+                .where(filter, prefilter=True) \
+                .to_pandas()
+            results.rename(columns={'_distance': 'section_relevance_score'}, inplace = True)
+
+        results.sort_values(by='section_relevance_score', ascending=False, inplace=True)
+        
+        return results
+
+    def safety_issue_search_with_report_relevance(self) -> pd.DataFrame:
+
+        where_statement = " AND ".join([
+            f"year >= {str(self.settings.getYearRange()[0])} AND year <= {str(self.settings.getYearRange()[1])}",
+            f"mode IN {tuple([mode.value for mode in self.settings.getModes()])}" if len(self.settings.getModes()) > 1 else f"mode = {self.settings.getModes()[0].value}"
+        ])
+        if self.query == "" or self.query is None:
+            return self.si_table.search().limit(None).where(where_statement, prefilter=True).to_pandas().assign(section_relevance_score = 0)
+        
+        report_sections_search_results = self._table_search(table = self.report_sections_table, limit = 50000, filter = where_statement, type = 'fts')
+        report_sections_search_results['section_relevance_score'] = (report_sections_search_results['section_relevance_score'] - report_sections_search_results['section_relevance_score'].min()) / (report_sections_search_results['section_relevance_score'].max() - report_sections_search_results['section_relevance_score'].min())
+        reports_relevance = report_sections_search_results.groupby('report_id').head(50).groupby('report_id')['section_relevance_score'].mean().sort_values(ascending=False).to_dict()
+        
+        safety_issues_search_results = self._table_search(table = self.si_table, type = 'vector', filter = where_statement, limit = 500)
+        safety_issues_search_results['section_relevance_score'] = safety_issues_search_results.apply(lambda row: row['section_relevance_score'] * (1-reports_relevance[row['report_id']] if row['report_id'] in reports_relevance else 1), axis = 1)
+
+        safety_issues_search_results.sort_values(by = 'section_relevance_score', inplace=True )
+
+        safety_issues_search_results.reset_index(drop=False, inplace=True)
+
+        return safety_issues_search_results
+
+    def _get_rag_prompt(self, query: str, context: str):
+        return f"""
+        Use the following pieces of retrieved context to answer the question. If you don't know the answer, just say that you don't know.
+        My question is: {query}
+
+        Here are relevant safety issues as context:
+        {context}
+
+        It is important to provide references to specific reports and safety issues in your answer.
+        """
+
+    def rag_search(self):
+
+        print((f"Understanding query..."))
+
+        formatted_query = openAICaller.query(
+            system = """
+    You are a helpful agent inside a RAG system.
+
+    You will receive a query from the user and return a query that should be sent to a vector database.
+
+    The database will search a dataset of safety issues from transport accident investigation reports.  It will use both embeddings and full text search.
+    """,
+            user = self.query,
+            model="gpt-4",
+            temp = 0.0
+        )
+        print(f' Going to run query: "{formatted_query}"')
+
+        print(f"Getting relevant safety issues...")
+
+        self.query = formatted_query
+        
+        search_results = self.safety_issue_search_with_report_relevance().head(50)
+
+        user_message = "\n".join(f"{id} from report {report} with relevance {rel} - {si}" for id, report, si, rel in zip(search_results['safety_issue_id'], search_results['report_id'], search_results['si'], search_results['section_relevance_score'])) 
+
+        print(f"Summarizing relevant safety issues...")
+        response = openAICaller.query(
+            system = """
+    You are a helpful AI that is part of a RAG system. You are going to help answer questions about transport accident investigations.
+
+    The questions are from investigators and researchers from the Transport Accident Investigation Commission. The context you will be given are safety issues extracted from all of TAICs reports.
+
+    A couple of useful definitions for you are:
+
+    Safety factor - Any (non-trivial) events or conditions, which increases safety risk. If they occurred in the future, these would
+    increase the likelihood of an occurrence, and/or the
+    severity of any adverse consequences associated with the
+    occurrence.
+
+    Safety issue - A safety factor that:
+    • can reasonably be regarded as having the
+    potential to adversely affect the safety of future
+    operations, and
+    • is characteristic of an organization, a system, or an
+    operational environment at a specific point in time.
+    Safety Issues are derived from safety factors classified
+    either as Risk Controls or Organisational Influences.
+
+    Safety theme - Indication of recurring circumstances or causes, either across transport modes or over time. A safety theme may
+    cover a single safety issue, or two or more related safety
+    issues.  
+    """,       
+        user=self._get_rag_prompt(self.query, user_message),
+        model="gpt-4",
+        temp = 0.2
+        )
+        return SearchResult(search_results, response)
