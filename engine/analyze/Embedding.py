1d49d83fecc5814b04baf67ff5eed787dcccebdf
1jamesthompson1 <1jamesthompson1@gmail.com>
2024-06-20 14:32:07 +1200

    Add useful report metadata to the embedding dataframes

--- engine/analyze/Embedding.py
+++ engine/analyze/Embedding.py
@@ -165,20 +165,42 @@ class Embedder:
             if isinstance(
                 extracted_df[dataframe_column_name].dropna().iloc[0], pd.DataFrame
             ):
+                filtered_extracted_df = extracted_df.dropna(
+                    subset=[dataframe_column_name]
+                )
                 dataframe_to_embed = pd.concat(
-                    list(extracted_df[dataframe_column_name].dropna()),
+                    [
+                        df.assign(report_id=report_id, type=type)
+                        for df, report_id, type in zip(
+                            filtered_extracted_df[dataframe_column_name],
+                            filtered_extracted_df["report_id"],
+                            filtered_extracted_df["type"],
+                        )
+                    ],
                     ignore_index=True,
                 )
             else:
                 dataframe_to_embed = extracted_df[
-                    ["report_id", dataframe_column_name]
+                    ["report_id", "type", dataframe_column_name]
                 ].dropna()
+
+            # Add mode and year to the embeddings
+            dataframe_to_embed["year"] = [
+                int(x.split("_")[0]) for x in dataframe_to_embed["report_id"]
+            ]
+            dataframe_to_embed["mode"] = [
+                x.split("_")[1] for x in dataframe_to_embed["report_id"]
+            ]
 
             if os.path.exists(output_file_path):
                 previously_embedded_df = pd.read_pickle(output_file_path)
                 dataframe_to_embed = dataframe_to_embed.merge(
                     previously_embedded_df,
-                    on=list(dataframe_to_embed.columns),
+                    on=list(
+                        set(dataframe_to_embed.columns).intersection(
+                            set(previously_embedded_df.columns)
+                        )
+                    ),
                     how="outer",
                 )
 
