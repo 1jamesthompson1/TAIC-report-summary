5841b3cd38d4f1ff1cf2ae2b574ba8304803d3e0
1jamesthompson1 <1jamesthompson1@gmail.com>
2024-06-21 13:30:04 +1200

    re run recommendation safety issue linking

--- tests/test_RecommendationSafetyIssueLinking.py
+++ tests/test_RecommendationSafetyIssueLinking.py
@@ -7,31 +7,32 @@ from engine.analyze.RecommendationSafetyIssueLinking import (
 )
 
 
-class TestRecommendationSafetyIssueLinking:
-    def get_test_dataframe(self):
-        return pd.read_pickle("tests/data/extracted_reports.pkl")
+def get_test_dataframe():
+    return pd.read_pickle("tests/data/extracted_reports.pkl")
 
-    def test_single_report_linking(self):
-        test_dataframe = self.get_test_dataframe()
 
-        links = RecommendationSafetyIssueLinker()._evaluate_all_possible_links(
-            test_dataframe["recommendations"].iloc[0],
-            test_dataframe["safety_issues"].iloc[0],
-        )
+def test_single_report_linking():
+    test_dataframe = get_test_dataframe()
 
-        assert isinstance(links, pd.DataFrame)
+    links = RecommendationSafetyIssueLinker()._evaluate_all_possible_links(
+        test_dataframe["recommendations"].iloc[0],
+        test_dataframe["safety_issues"].iloc[0],
+    )
 
-    def test_multiple_reports_linking(self):
-        RecommendationSafetyIssueLinker().evaluate_links_for_report(
-            "tests/data/extracted_reports.pkl", "tests/data/links_temp.pkl"
-        )
+    assert isinstance(links, pd.DataFrame)
 
-        assert os.path.exists("tests/data/links_temp.pkl")
 
-        dataframe = pd.read_pickle("tests/data/links_temp.pkl")
+def test_multiple_reports_linking(tmpdir):
+    output_path = tmpdir.join("links_temp.pkl")
 
-        assert isinstance(dataframe, pd.DataFrame)
-        assert dataframe.columns.tolist() == ["report_id", "recommendation_links"]
-        assert isinstance(dataframe.loc[0, "recommendation_links"], pd.DataFrame)
+    RecommendationSafetyIssueLinker().evaluate_links_for_report(
+        "tests/data/extracted_reports.pkl", output_path
+    )
 
-        os.remove("tests/data/links_temp.pkl")
+    assert os.path.exists(output_path)
+
+    dataframe = pd.read_pickle(output_path)
+
+    assert isinstance(dataframe, pd.DataFrame)
+    assert dataframe.columns.tolist() == ["report_id", "recommendation_links"]
+    assert isinstance(dataframe.loc[0, "recommendation_links"], pd.DataFrame)
