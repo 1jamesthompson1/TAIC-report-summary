e38fdab1db7c0e98c88a94ca3c00c068aa278229
1jamesthompson1 <1jamesthompson1@gmail.com>
2024-07-03 19:27:55 +1200

    Adding in basic login. Works locally using my app registration on my azure instance.

--- viewer/app.py
+++ viewer/app.py
@@ -2,12 +2,81 @@ import argparse
 import os
 import tempfile
 
+import identity.web
 import pandas as pd
-from flask import Flask, g, jsonify, render_template, request, send_file
+from flask import (
+    Flask,
+    g,
+    jsonify,
+    redirect,
+    render_template,
+    request,
+    send_file,
+    session,
+    url_for,
+)
+from werkzeug.middleware.proxy_fix import ProxyFix
 
-from . import Searching
+from flask_session import Session
+
+from . import Searching, app_config
+
+__version__ = "1.0.0-beta"
 
 app = Flask(__name__)
+app.config.from_object(app_config)
+assert app.config["REDIRECT_PATH"] != "/", "REDIRECT_PATH must not be /"
+Session(app)
+
+
+app.wsgi_app = ProxyFix(app.wsgi_app, x_proto=1, x_host=1)
+
+app.jinja_env.globals.update(Auth=identity.web.Auth)  # Useful in template for B2C
+auth = identity.web.Auth(
+    session=session,
+    authority=app.config["AUTHORITY"],
+    client_id=app.config["CLIENT_ID"],
+    client_credential=app.config["CLIENT_SECRET"],
+)
+
+
+@app.route("/login")
+def login():
+    return render_template(
+        "login.html",
+        version=__version__,
+        **auth.log_in(
+            scopes=app_config.SCOPE,  # Have user consent to scopes during log-in
+            redirect_uri=url_for(
+                "auth_response", _external=True
+            ),  # Optional. If present, this absolute URL must match your app's redirect_uri registered in Microsoft Entra admin center
+            prompt="select_account",  # Optional.
+        ),
+    )
+
+
+@app.route(app_config.REDIRECT_PATH)
+def auth_response():
+    result = auth.complete_log_in(request.args)
+    if "error" in result:
+        return render_template("auth_error.html", result=result)
+    return redirect(url_for("index"))
+
+
+@app.route("/logout")
+def logout():
+    return redirect(auth.log_out(url_for("index", _external=True)))
+
+
+@app.route("/")
+def index():
+    if not (app.config["CLIENT_ID"] and app.config["CLIENT_SECRET"]):
+        # This check is not strictly necessary.
+        # You can remove this check from your production code.
+        return render_template("config_error.html")
+    if not auth.get_user():
+        return redirect(url_for("login"))
+    return render_template("index.html", user=auth.get_user(), version=__version__)
 
 
 def get_searcher():
@@ -15,11 +84,6 @@ def get_searcher():
         g.searcher = Searching.SearchEngine(os.environ["db_URI"])
 
     return g.searcher
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
 
 
 def get_search(form) -> Searching.Search:
@@ -109,8 +173,7 @@ def run():
 
     os.environ["db_URI"] = "./viewer/vector_db"
 
-    port = int(os.environ.get("PORT", 5001))
-    app.run(port=port, host="0.0.0.0", debug=args.debug)
+    app.run(port=5000, host="localhost", debug=args.debug)
 
 
 if __name__ == "__main__":
