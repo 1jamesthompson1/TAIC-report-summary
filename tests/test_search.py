6705d954d4acd410da43d039570545f7eac70e2a
1jamesthompson1 <1jamesthompson1@gmail.com>
2024-07-27 12:42:42 +1200

    Update tests

--- tests/test_search.py
+++ tests/test_search.py
@@ -1,3 +1,5 @@
+import urllib.parse
+
 import pandas as pd
 import pytest
 
@@ -27,6 +29,38 @@ class TestSearchSettings:
 
         with pytest.raises(TypeError):
             Searching.SearchSettings(["a", "r"], (2001, 2020))
+
+    def test_to_dict(self):
+        settings = Searching.SearchSettings(
+            [Modes.Mode.a, Modes.Mode.r],
+            (2000, 2020),
+            ["safety_issue", "recommendation"],
+            0.1,
+        )
+
+        assert settings.to_dict() == {
+            "setting_modes": "[0, 1]",
+            "setting_max_year": 2020,
+            "setting_min_year": 2000,
+            "setting_document_types": "['safety_issue', 'recommendation']",
+            "setting_relevanceCutoff": 0.1,
+        }
+
+    def test_from_dict(self):
+        settings = Searching.SearchSettings.from_dict(
+            {
+                "setting_modes": "[0, 1]",
+                "setting_max_year": 2020,
+                "setting_min_year": 2000,
+                "setting_document_types": "['safety_issue', 'recommendation']",
+                "setting_relevanceCutoff": 0.1,
+            }
+        )
+
+        assert settings.getModes() == [Modes.Mode.a, Modes.Mode.r]
+        assert settings.getYearRange() == (2000, 2020)
+        assert settings.getDocumentTypes() == ["safety_issue", "recommendation"]
+        assert settings.getRelevanceCutoff() == 0.1
 
 
 class TestSearch:
@@ -73,6 +107,33 @@ class TestSearch:
 
         with pytest.raises(ValueError):
             Searching.Search.from_form(form)
+
+    def test_to_url_params(self):
+        search = Searching.Search(
+            "hello",
+            Searching.SearchSettings(
+                [Modes.Mode.a, Modes.Mode.r],
+                (2000, 2020),
+                ["safety_issue", "recommendation"],
+                0.1,
+            ),
+        )
+
+        assert isinstance(search.to_url_params(), str)
+
+        parsed_params = urllib.parse.parse_qs(search.to_url_params())
+
+        assert parsed_params["searchQuery"][0] == "hello"
+        assert parsed_params["includeModeAviation"][0] == "on"
+        assert parsed_params["includeModeRail"][0] == "on"
+        assert parsed_params["includeModeMarine"][0] == "off"
+        assert parsed_params["yearSlider-min"][0] == "2000"
+        assert parsed_params["yearSlider-max"][0] == "2020"
+        assert parsed_params["relevanceCutoff"][0] == "0.1"
+        assert parsed_params["includeSafetyIssues"][0] == "on"
+        assert parsed_params["includeRecommendations"][0] == "on"
+        assert parsed_params["includeReportSection"][0] == "off"
+        assert parsed_params["includeImportantText"][0] == "off"
 
 
 class TestSearcher:
