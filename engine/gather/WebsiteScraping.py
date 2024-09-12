2a8a410aeee01dc21693605ef5cabdcc707dd229
1jamesthompson1 <1jamesthompson1@gmail.com>
2024-09-12 15:55:28 +1200

    Add in TSB and ATSB while restructuring the report collection process. Note that ATSB is still not working

--- engine/gather/WebsiteScraping.py
+++ engine/gather/WebsiteScraping.py
@@ -1,4 +1,5 @@
 import os
+from urllib.parse import urljoin, urlparse
 
 import pandas as pd
 import requests
@@ -30,7 +31,6 @@ class ReportScraperSettings:
         self.refresh = refresh
         self.modes = modes
         self.ignored_report_ids = ignored_report_ids
-        self.agency = "Undefined"
 
 
 class ReportScraper:
@@ -49,7 +49,20 @@ class ReportScraper:
                 self.settings.report_titles_file_path
             )
         else:
-            self.report_titles_df = pd.DataFrame(columns=["report_id", "title"])
+            self.report_titles_df = pd.DataFrame(
+                columns=["report_id", "title", "event_type"]
+            )
+
+        self.headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:91.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36 Edg/94.0.992.50",
+            "Accept-Language": "en-US,en;q=0.9",
+            "Referer": "https://www.google.com/",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Connection": "keep-alive",
+        }
+
+        # Create a folder to store the downloaded PDFs
+        os.makedirs(self.settings.report_dir, exist_ok=True)
 
     def collect_all(self):
         print(
@@ -76,16 +89,18 @@ class ReportScraper:
         print(f"  Modes: {self.settings.modes}")
         print(f"  Ignoring report ids: {self.settings.ignored_report_ids}")
 
-        # Create a folder to store the downloaded PDFs
-        if not os.path.exists(self.settings.report_dir):
-            os.makedirs(self.settings.report_dir)
-
         # Loop through each mode
         for mode in self.settings.modes:
             self.collect_mode(mode)
 
-    def get_url_from_template(self, mode, year, num):
+    def get_report_urls(self, mode: str, year: int) -> list[tuple[str, str]]:
+        """
+        Retrieves all the potential report urls and ids for a given mode and year
+        """
         raise NotImplementedError
+
+    def get_report_id(self, mode: Modes.Mode, year: int, id: str) -> str:
+        return f"{self.agency}_{mode.name}_{year}_{id}"
 
     def collect_mode(self, mode):
         print(f"======== Downloading reports for mode: {mode.name}==========")
@@ -103,14 +118,9 @@ class ReportScraper:
 
     def collect_year(self, year, mode):
         # Define the base URL and report ids and download all reports for the mode.
-        mode_id_base = mode.value * 100 + 1
-        id_range = ["{0:03d}".format(i) for i in range(mode_id_base, mode_id_base + 99)]
 
         number_for_year = 0
-        for i in (inner_pbar := tqdm(id_range)):
-            url = self.get_url_from_template(mode.name, year, i)
-            report_id = f"{self.agency}_{mode.name}_{year}_{i}"
-
+        for report_id, url in (inner_pbar := tqdm(self.get_report_urls(mode, year))):
             if report_id in self.settings.ignored_report_ids:
                 continue
 
@@ -131,39 +141,54 @@ class ReportScraper:
             self.settings.report_dir,
             self.settings.file_name_template.replace(r"{{report_id}}", report_id),
         )
+        if pbar:
+            pbar.set_description(f"  Collecting {url}")
 
         if not self.settings.refresh and os.path.exists(file_name):
             if pbar:
                 pbar.set_description(f"  {file_name} already exists, skipping download")
             return True
-        webpage = requests.get(url)
 
+        try:
+            webpage = requests.get(url, headers=self.headers, timeout=10)
+        except requests.exceptions.Timeout:
+            if pbar:
+                pbar.write(f"  Failed to collect {url}, timeout error")
+            return False
         soup = BeautifulSoup(webpage.content, "html.parser")
+
+        if webpage.status_code != 200 and webpage.status_code != 404:
+            if pbar:
+                pbar.write(
+                    f"  Failed to collect {url}, status code: {webpage.status_code}"
+                )
+            return False
+        elif webpage.status_code == 404:
+            return False
 
         if soup.find("h1", text="Page not found"):
             return "End of reports for this year"
 
-        outcome = self.download_report(report_id, file_name, soup, pbar)
+        parsed_url = urlparse(url)
+        base_url = f"{parsed_url.scheme}://{parsed_url.netloc}"
+
+        outcome = self.download_report(report_id, file_name, soup, base_url, pbar)
         if not outcome:
             return False
 
-        self.get_report_title(report_id, soup, pbar)
+        self.__add_report_metadata_to_df(
+            *self.get_report_metadata(report_id, soup, pbar)
+        )
 
         return True
 
-    def get_report_title(self, report_id: str, soup: BeautifulSoup, pbar=None):
-        title = soup.find("div", class_="field--name-field-inv-title").text
-
-        if self.report_titles_df.query("report_id == @report_id").empty:
-            self.report_titles_df.loc[len(self.report_titles_df)] = [
-                report_id,
-                title,
-            ]
-
-        pass
-
     def download_report(
-        self, report_id: str, file_name: str, soup: BeautifulSoup, pbar=None
+        self,
+        report_id: str,
+        file_name: str,
+        soup: BeautifulSoup,
+        base_url: str,
+        pbar=None,
     ):
         # Find all the links that end with .pdf and download them
 
@@ -174,6 +199,10 @@ class ReportScraper:
         ]
 
         if len(pdf_links) == 0:
+            if pbar:
+                pbar.write(
+                    f"WARNING: Found no suitable PDF link for {report_id}. Will not download any."
+                )
             return False
         if len(pdf_links) > 1:
             # Only take one that has "final" but not "interim"
@@ -197,10 +226,17 @@ class ReportScraper:
                     )
                 return False
 
-        link = pdf_links[0]
+        link = urljoin(base_url, pdf_links[0])
+        if pbar:
+            pbar.set_description(f"  Downloading {link}")
+
         try:
             with open(file_name, "wb") as f:
-                f.write(requests.get(link, allow_redirects=True, timeout=10).content)
+                f.write(
+                    requests.get(
+                        link, allow_redirects=True, headers=self.headers, timeout=10
+                    ).content
+                )
                 if pbar:
                     pbar.set_description(f"  Downloaded {file_name}")
         except requests.ReadTimeout:
@@ -211,18 +247,114 @@ class ReportScraper:
 
         return True
 
+    def get_report_metadata(self, report_id: str, soup: BeautifulSoup, pbar=None):
+        raise NotImplementedError
+
+    def __add_report_metadata_to_df(self, report_id: str, title: str, event_type: str):
+        if self.report_titles_df.query("report_id == @report_id").empty:
+            self.report_titles_df.loc[len(self.report_titles_df)] = [
+                report_id,
+                title,
+                event_type,
+            ]
+
 
 class TAICReportScraper(ReportScraper):
     def __init__(self, settings: ReportScraperSettings):
         super().__init__(settings)
         self.agency = "TAIC"
 
-    def get_url_from_template(self, mode, year, num):
-        return f"https://www.taic.org.nz/inquiry/{mode}o-{year}-{num}"
+    def get_report_urls(self, mode, year):
+        taic_ids = [f"{mode.value}{num:02}" for num in range(100)]
+        return [
+            (
+                self.get_report_id(mode, year, taic_id),
+                f"https://www.taic.org.nz/inquiry/{mode.name}o-{year}-{taic_id}",
+            )
+            for taic_id in taic_ids
+        ]
+
+    def get_report_metadata(self, report_id: str, soup: BeautifulSoup, pbar=None):
+        title = soup.find("div", class_="field--name-field-inv-title").text
+
+        return report_id, title, None
+
+
+class ATSBReportScraper(ReportScraper):
+    def __init__(self, settings: ReportScraperSettings):
+        super().__init__(settings)
+        self.agency = "ATSB"
+
+    def get_report_urls(self, mode, year):
+        atsb_ids = [f"{mode.value}{num:02}" for num in range(100)]
+        return [
+            (
+                self.get_report_id(mode, year, atsb_id),
+                f"https://www.atsb.gov.au/publications/investigation_reports/{year}/report/{mode.name}o-{year}-{atsb_id}",
+            )
+            for atsb_id in atsb_ids
+        ]
+
+
+class TSBReportScraper(ReportScraper):
+    def __init__(self, settings: ReportScraperSettings):
+        super().__init__(settings)
+        self.agency = "TSB"
+
+    def __get_tsb_mode_letters(self, mode):
+        match mode:
+            case Modes.Mode.a:
+                return ["A", "W", "O", "C", "P", "Q"]
+
+            case Modes.Mode.r:
+                return ["H", "T", "W", "C", "D", "V", "M", "E", "S", "Q", "H"]
+
+            case Modes.Mode.m:
+                return ["C", "A", "P", "F", "M", "H", "L", "W", "N"]
+
+    def get_report_urls(self, mode, year):
+        yy_year = year % 100
+        tsb_id = [
+            f"{mode.name}{yy_year}{letter}{num:04}"
+            for letter in self.__get_tsb_mode_letters(mode)
+            for num in range(500)
+        ]
+
+        return [
+            (
+                self.get_report_id(mode, year, tsb_id[-5:]),
+                f"https://www.tsb.gc.ca/eng/rapports-reports/{Modes.Mode.as_string(mode)}/{year}/{tsb_id}/{tsb_id}.html".lower(),
+            )
+            for tsb_id in tsb_id
+        ]
+
+    def get_report_metadata(self, report_id: str, soup: BeautifulSoup, pbar=None):
+        title_block = soup.find("div", class_="field--name-field-occurrence")
+
+        event_type = title_block.find("strong").text
+
+        legacy_text_div = title_block.find(
+            "div", class_="field--name-field-occurrence-legacy-text"
+        )
+        paragraph = legacy_text_div.find("p")
+        paragraph_text = [text for text in paragraph.stripped_strings]
+        date_text = (
+            title_block.find("div", class_="field--name-field-occurrence-date")
+            .find("time")
+            .text
+        )
+
+        all_text = ", ".join(paragraph_text + [date_text])
+
+        return report_id, all_text, event_type
 
 
 def get_agency_scraper(agency: str, settings: ReportScraperSettings) -> ReportScraper:
     if agency == "TAIC":
         return TAICReportScraper(settings)
+    elif agency == "ATSB":
+        return ATSBReportScraper(settings)
+    elif agency == "TSB":
+        return TSBReportScraper(settings)
     else:
         raise ValueError(f"Unknown agency: {agency}")
--- tests/test_website_scrapping.py
+++ tests/test_website_scrapping.py
@@ -7,6 +7,64 @@ import engine.gather.WebsiteScraping as WebsiteScraping
 import engine.utils.Modes as Modes
 
 
+@pytest.fixture(scope="function")
+def report_scraping_settings(tmpdir):
+    return WebsiteScraping.ReportScraperSettings(
+        os.path.join(tmpdir, "report_folder"),
+        os.path.join(tmpdir, "report_titles_df.pkl"),
+        "{{report_id}}.pdf",
+        2010,
+        2020,
+        1,
+        [Modes.Mode.a],
+        [],
+        False,
+    )
+
+
+@pytest.mark.parametrize(
+    "agency, url, expected",
+    [
+        pytest.param(
+            "TSB",
+            "https://www.tsb.gc.ca/eng/rapports-reports/marine/2020/m20c0101/m20c0101.html",
+            True,
+            id="TSB pass",
+        ),
+        pytest.param(
+            "TSB",
+            "https://www.tsb.gc.ca/eng/rapports-reports/marine/2020/m20c0101/m20c0102.html",
+            False,
+            id="TSB fail",
+        ),
+        pytest.param(
+            "TAIC", "https://www.taic.org.nz/inquiry/mo-2021-205", True, id="TAIC pass"
+        ),
+        pytest.param(
+            "TAIC", "https://www.taic.org.nz/inquiry/mo-2021-255", False, id="TAIC fail"
+        ),
+        pytest.param(
+            "ATSB",
+            "https://www.atsb.gov.au/publications/investigation_reports/2019/mair/mo-2019-007",
+            True,
+            id="ATSB pass",
+        ),
+        pytest.param(
+            "ATSB",
+            "https://www.atsb.gov.au/publications/investigation_reports/2019/mair/mo-2019-008",
+            False,
+            id="ATSB fail",
+        ),
+    ],
+)
+def test_report_collection(report_scraping_settings, agency, url, expected):
+    scraper = WebsiteScraping.get_agency_scraper(agency, report_scraping_settings)
+
+    result = scraper.collect_report("test_report_id", url)
+
+    assert result == expected
+
+
 def test_collect_all(tmpdir):
     report_folder = tmpdir.join("report_folder")
     report_titles = tmpdir.join("report_titles_df.pkl")
