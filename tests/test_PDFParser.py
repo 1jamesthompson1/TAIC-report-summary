74bf2a73a8c6943cb4a5c40647be23e97a697b6a
James Thompson <1jamesthompson1@gmail.com>
2024-10-24 15:05:40 +1300

    257-allow-pdf-parsing-to-text-to-handle-atsb-and-tsb-reports (#264)
    resolve #257
    * I have about 90% accurate page number extraction from the ATSB reports.
    
    However I can do a near perfect job if I use the PDF reader to actually extract the page number directly. Then I calibrate it by finding the written page numbers in the report.
    
    * Add in the usage of the pdf pages to sync the regex page numbers.
    
    I need to add testing to this and fix some bugs.
    
    * Add some tests and finish text extraction of ATSB with about a 5-10% error rate.
    
    I will try to add in TSB and see how it goes. Lastly I need to move over taic to this as I have added page numbers in the text extraction stage.
    
    * Add in support for TSB page number extraction
    
    * Completed TAIC into the same framework. Now all three agencies are added.
    
    A final check of the page extraction should be done.
    
    * Final checks and removing printouts
    
    There is over a 97% success rate.
    I have spot checked and the validator seems to work as one would expect.
    
    * Update test

---
+++ tests/test_PDFParser.py
@@ -0,0 +1,164 @@
+import os
+import re
+
+import pandas as pd
+import pytest
+
+import engine.gather.PDFParser as PDFParser
+
+
+@pytest.mark.parametrize(
+    "report_id, expected",
+    [
+        pytest.param(
+            "ATSB_a_2007_030",
+            [
+                "i",
+                "ii",
+                "iii",
+                "iv",
+                "v",
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+                21,
+                22,
+            ],
+            id="ATSB_a_2007_030 (Incorrect roman numeral matches in text)",
+        ),
+        pytest.param(
+            "ATSB_a_2002_646",
+            [
+                "i",
+                "ii",
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+                17,
+                18,
+                19,
+                20,
+            ],
+            id="ATSB_a_2002_646 (Lenient regex match of roman numerals causing error)",
+        ),
+        pytest.param(
+            "TSB_m_2021_A0041", [i for i in range(1, 57)], id="TSB_m_2021_A0041"
+        ),
+        pytest.param(
+            "TSB_a_2011_F0012", [i for i in range(1, 20)], id="TSB_a_2011_F0012"
+        ),
+        pytest.param(
+            "TAIC_r_2004_121",
+            [
+                "i",
+                "ii",
+                "iii",
+                "iv",
+                1,
+                2,
+                3,
+                4,
+                5,
+                6,
+                7,
+                8,
+                9,
+                10,
+                11,
+                12,
+                13,
+                14,
+                15,
+                16,
+            ],
+            id="TAIC_r_2004_121",
+        ),
+        pytest.param(
+            "TAIC_r_2014_103",
+            ["i", "ii", "iii", "iv"] + list(range(1, 39)),
+            id="TAIC_r_2014_103",
+        ),
+        pytest.param(
+            "TAIC_a_2019_006",
+            ["i", "ii", "iii", "iv", "v", "vi"] + list(range(1, 65)),
+            id="TAIC_a_2019_006 (removing duplicate matches on the same page)",
+        ),
+    ],
+)
+def test_formatText(report_id, expected):
+    text = PDFParser.extractTextFromPDF(
+        os.path.join(
+            pytest.output_config["folder_name"],
+            pytest.output_config["report_pdf_folder_name"],
+            f"{report_id}.pdf",
+        )
+    )
+
+    text, valid = PDFParser.formatText(text, report_id)
+
+    page_number_matches = list(
+        re.finditer(
+            r"^<< Page (\d+|[LXVI]{1,8}) >>$", text, re.MULTILINE + re.IGNORECASE
+        )
+    )
+
+    assert valid
+
+    matched = [
+        int(match.group(1)) if match.group(1).isnumeric() else match.group(1)
+        for match in page_number_matches
+    ]
+    print(f"found: {matched}")
+    print(f"expected: {expected}")
+    assert matched == expected
+
+
+def test_PDFParser(tmpdir):
+    report_pdfs_folder = os.path.join(
+        pytest.output_config["folder_name"],
+        pytest.output_config["report_pdf_folder_name"],
+    )
+
+    parsed_reports_df_file_name = os.path.join(
+        tmpdir.strpath,
+        pytest.output_config["parsed_reports_df_file_name"],
+    )
+
+    PDFParser.convertPDFToText(report_pdfs_folder, parsed_reports_df_file_name, True)
+
+    assert os.path.exists(parsed_reports_df_file_name)
+
+    parsed_reports_df = pd.read_pickle(parsed_reports_df_file_name)
+    print(parsed_reports_df)
+
+    assert parsed_reports_df["valid"].all()
