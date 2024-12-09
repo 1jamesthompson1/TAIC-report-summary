cabed1b647034b2d2cb2078481b72da269ea371c
1jamesthompson1 <1jamesthompson1@gmail.com>
2024-12-09 20:51:55 +1300

    Fix up errors

--- tests/test_website_scrapping.py
+++ tests/test_website_scrapping.py
@@ -107,7 +107,7 @@ def test_report_collection(report_scraping_settings, agency, url, report_id, exp
     "agency, expected_urls",
     [
         pytest.param("TSB", [54, 24, 20, 13, 19, 10, 15, 10, 13], id="TSB"),
-        pytest.param("TAIC", [11, 12, 3, 29, 8, 4, 12, 3, 5], id="TAIC"),
+        pytest.param("TAIC", [11, 12, 3, 28, 8, 4, 12, 3, 5], id="TAIC"),
         pytest.param("ATSB", [93, 179, 50, 6, 25, 17, 15, 8, 2], id="ATSB"),
     ],
 )
@@ -143,15 +143,18 @@ def test_agency_website_scraper(report_scraping_settings, agency, expected_urls)
 @pytest.mark.parametrize(
     "agency, expected_count",
     [
-        pytest.param("TSB", 33, id="TSB"),
-        pytest.param("TAIC", 33, id="TAIC"),
-        pytest.param("ATSB", 33, id="ATSB"),
+        pytest.param("TSB", 15, id="TSB"),
+        pytest.param("TAIC", 15, id="TAIC"),
+        pytest.param("ATSB", 15, id="ATSB"),
     ],
 )
 def test_agency_website_scraper_collecting_all_reports(
     report_scraping_settings, agency, expected_count
 ):
     report_scraping_settings.refresh = True
+
+    report_scraping_settings.start_year = 2008
+    report_scraping_settings.end_year = 2012
 
     scraper = get_agency_scraper(agency, report_scraping_settings)
 
